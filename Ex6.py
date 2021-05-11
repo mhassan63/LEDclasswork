84b4ceee58d1883cbc38700a56c9fa056b684676
mhassan63 <mhassan@tfssa.org>
2021-05-11 09:59:47 -0500

    Ex6

--- Ex6.py
+++ Ex6.py
@@ -3,4 +3,10 @@ import neopixel
 
 pixels = neopixel.NeoPixel(board.D18, 20)
 
-pixels[16] = (127,3,252)
+#red
+for i in range (6):
+    pixles[i] = (10,0,0)
+
+#blue
+for i in range (5,20):
+    pixles[i] = (0,0,252)
